73f6cd56e414b31e4d616a469dc5873dc39c3f9f
shawnzx <shawnzx@Ubuntu.myguest.virtualbox.org>
2023-04-29 10:48:15 -0400

    Fixed the damn button not working...sort of.

--- myscripts.js
+++ myscripts.js
@@ -1,13 +1,19 @@
 // Get the button and sidebar elements
 const sidebarToggle = document.getElementById('sidebar-toggle');
-const sidebar = document.getElementById('sidebar');
+const sidebar = document.getElementsByClassName('side-bar')[0];
 
 // Add a click event listener to the button
-sidebarToggle.addEventListener('click', function() {
+sidebarToggle.addEventListener('click', toggleSidebar);
   // Toggle the visibility of the sidebar by adding or removing the 'hidden' class
+
+
+
+
+function toggleSidebar() {
   if (sidebar.classList.contains('hidden')) {
     sidebar.classList.remove('hidden');
   } else {
     sidebar.classList.add('hidden');
   }
-});
+}
+// Path: myscripts.js
